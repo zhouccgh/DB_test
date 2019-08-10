a39239ada91b43f6277f8fb1aa1b5bde59c17aec
zhouccgh <2875916798@qq.com>
2019-08-10 14:38:58 +0800

    1

--- src/test/java/com/test/git/TestGit.java
+++ src/test/java/com/test/git/TestGit.java
@@ -5,10 +5,10 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("第一次测试");
 		System.out.println("2");
-<<<<<<< HEAD
+
 		System.out.println("3");
-=======
+
 		System.out.println("4");
->>>>>>> branch 'master' of https://github.com/zhouccgh/DB_test.git
+
 	}
 }
